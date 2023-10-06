3c97a115273fa3010bc48e719a517428915ef03d
jingggyiii <jyhuang.2020@smu.edu.sg>
2023-10-06 19:57:42 +0800

    ForgtPw modal

--- oop/src/components/UserLogin.vue
+++ oop/src/components/UserLogin.vue
@@ -4,28 +4,89 @@
             <div class="logo">
                 <img src="../assets/GSLogo.png" alt="Vue" class="app_logo">
             </div>
-            <!-- <p class="pb-1">Goldman Sachs</p> -->
+
             <h3>Welcome Back</h3>
-            <p>Enter your email and password below</p>
+            <p class = "grey_text">Enter your email and password below</p>
             <!-- Email input -->
             <div class="form-outline mb-4">
-                <label class="form-label" for="form2Example1">Email</label>
-                <input type="email" id="form2Example1" class="form-control" v-model="email"/>
+                <label class="form-label grey_text" for="form2Example1">Email</label>
+                <input type="email" id="form2Example1" class="form-control grey_text" v-model="email" placeholder="Enter Email Address"/>
             </div>
 
             <!-- Password input -->
             <div class="form-outline mb-4">
-                <label class="form-label" for="form2Example2">Password</label>
-                <label class="pwd" @click="ForgetPassword">Forget Password ?</label>
+                <label class="form-label grey_text" for="form2Example2">Password</label>
+
+                <!-- Forget Password -->
+                <label class="pwd grey_text" data-bs-toggle="modal" data-bs-target="#forgetpasswordModal"><u>Forget Password?</u></label>
+                
                 <div class="d-flex">
-                    <input type="password" id="form2Example2" class="form-control" v-model="pwd"/>
+                    <input type="password" id="form2Example2" class="form-control" v-model="pwd" placeholder="Enter Password"/>
                     <span class="material-symbols-outlined" @click="ShowHide()">{{visible}}</span>
                 </div>
+
             </div>
 
             <!-- Submit button -->
             <button type="button" class="login_btn my-4 p-2" @click="getUserType()">Log in</button>
         </form>
+
+        <!-- modal for forget pw -->
+        <div class="modal fade" id="forgetpasswordModal" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h3 style="text-align: center">Change Password</h3>
+                        <button
+                            type="btn"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-hidden="true"
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <p>
+                                        If you have forgotten your password you can reset it here.
+                                    </p>
+                                    <div class="panel-body">
+                                        <fieldset>
+                                            <div class="form-group row">
+                                                <label
+                                                    for="forgetPw"
+                                                    class="col-sm-2 col-form-label" 
+                                                    >Email</label
+                                                >
+                                                <div class="col-sm-10">
+                                                    <input
+                                                        type="email"
+                                                        class="form-control"
+                                                        id="forgetPw"
+                                                        placeholder="Enter Email Address"
+                                                        v-model="email"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <button
+                                                type="button"
+                                                class="send_btn my-4 p-2"
+                                                @click="sendEmail()"
+                                                id="close"
+                                            >
+                                                Send Password
+                                            </button>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -45,7 +106,7 @@ export default {
         getUserType() {
             localStorage.setItem("usertype", this.staff)
             // sessionStorage.setItem("staff_id", staff.staff_id)
-            this.$router.push('/consumption')
+            this.$router.push('/home')
         },
         ForgetPassword() {
             this.$router.push('/forget_password')
@@ -92,7 +153,7 @@ form {
 .form-outline {
     text-align: left;
 }
-.login_btn {
+.login_btn, .send_btn {
     width: 100%;
     background-color: #7399C6;
     color: white;
@@ -105,5 +166,13 @@ form {
 .material-symbols-outlined {
     margin-left: -45px;
     padding: 8px;
+}
+
+.grey_text {
+    color: var(--light-grey);
+}
+
+.modal {
+    top: 25%;
 }
 </style>
