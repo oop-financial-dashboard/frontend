9d185e098d390b6860134ec576c642e72d3f21a3
sherrillxtan <sherrilltan.2020@scis.smu.edu.sg>
2023-10-27 15:33:00 +0800

    Revert "Merge branch 'main' into Sherrill"
    This reverts commit 596cb9e4f8d9a1fde6a83dc6545c78ab9bb63170, reversing
    changes made to a51bf41e99c303e9d74c3f09f3acdd61395b6c15.

--- oop/src/views/SettingsPage.vue
+++ oop/src/views/SettingsPage.vue
@@ -1,9 +1,7 @@
 <template>
   <main class="aboutpage">
     <h1>Admin Settings</h1>
-    <p>This is the settings page</p>
-    <button type="button" class="btn btn btn-outline-dark" @click="ChangePassword()">
-    <!-- <button type="button" class="forget_btn my-4 p-2" @click="ChangePassword()"> -->
+    <button type="button" class="btn btn-dark" @click="ChangePassword()">
         Change Password
     </button>
 </main>
