6775bc3667609ea56d7d68e0a183a7c84446f167
jingggyiii <jingggyiii@gmail.com>
2023-10-27 20:32:29 +0800

    Added spinner

--- oop/src/views/ConfirmAccount.vue
+++ oop/src/views/ConfirmAccount.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-      <h1> Hi </h1>
-      <p>Token: {{ token }}</p>
-    </div>
+      <div class = "cfmAcc">
+      <h3>Your account has been verified!</h3>
+      <p class="grey_text">You will be redirected to login page in 5 seconds</p>
+      <div class="spinner-border" role="status">
+        <span class="sr-only"></span>
+      </div>   
+      </div>
   </template>
   
 <script>
@@ -16,12 +19,33 @@ export default {
     axios.get(`/auth/confirm-account?token=${this.token}`)
       .then((response) => {
         if (response.status === 200) {
-          this.$router.push("/");
+          setTimeout(() => {
+            this.$router.push("/");
+          }, 5000)
         }
       });
-  }
+  },
 };
 </script>
   
 <style scoped>
+p {
+  font-size: 14px;
+}
+
+.cfmAcc {
+  width: 30%;
+  background-color: var(--lighter-grey-alt);
+  padding: 40px;
+  border-radius: 5px;
+  text-align: center;
+  margin: auto;
+  margin-top: 10%;
+  border: 2px solid #7399c6;;
+}
+
+.form-outline {
+  text-align: left;
+}
+
 </style>
