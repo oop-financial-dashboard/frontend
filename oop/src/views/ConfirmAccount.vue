d247a360ed59e535134f8f2b4a06384a074b836b
jingggyiii <jingggyiii@gmail.com>
2023-10-27 20:44:01 +0800

    redirect done

--- oop/src/views/ConfirmAccount.vue
+++ oop/src/views/ConfirmAccount.vue
@@ -1,33 +1,50 @@
 <template>
-      <div class = "cfmAcc">
-      <h3>Your account has been verified!</h3>
-      <p class="grey_text">You will be redirected to login page in 5 seconds</p>
-      <div class="spinner-border" role="status">
-        <span class="sr-only"></span>
-      </div>   
-      </div>
-  </template>
-  
+  <div class="cfmAcc">
+    <h3>Your account has been verified!</h3>
+    <p class="grey_text">
+      You will be redirected to login page in {{ countdown }} seconds
+    </p>
+    <div class="spinner-border" role="status">
+      <span class="sr-only"></span>
+    </div>
+  </div>
+</template>
+
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'ConfirmAccount',
-  props: ['token'],
+  data() {
+    return {
+      countdown: 5,
+    };
+  },
+  name: "ConfirmAccount",
+  props: ["token"],
   created() {
-    console.log(this.token);
-    axios.get(`/auth/confirm-account?token=${this.token}`)
-      .then((response) => {
-        if (response.status === 200) {
-          setTimeout(() => {
-            this.$router.push("/");
-          }, 5000)
+    axios.get(`/auth/confirm-account?token=${this.token}`).then((response) => {
+      if (response.status === 200) {
+        this.startCountdown();
+      }
+    });
+  },
+  methods: {
+    startCountdown() {
+      const countdownInterval = setInterval(() => {
+        this.countdown--;
+        if (this.countdown === 0) {
+          clearInterval(countdownInterval);
+          this.redirectToLogin();
         }
-      });
+      }, 1000);
+    },
+    redirectToLogin() {
+      this.$router.push("/");
+    },
   },
 };
 </script>
-  
+
 <style scoped>
 p {
   font-size: 14px;
@@ -41,11 +58,6 @@ p {
   text-align: center;
   margin: auto;
   margin-top: 10%;
-  border: 2px solid #7399c6;;
+  border: 2px solid #7399c6;
 }
-
-.form-outline {
-  text-align: left;
-}
-
 </style>
