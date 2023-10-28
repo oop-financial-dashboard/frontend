925d2e694ff91c2e59343dc86e7c9924f42dd01c
xuezhen01 <xuezhen.ng.2020@scis.smu.edu.sg>
2023-10-28 21:06:16 +0800

    small data box

---
+++ oop/src/components/dataBox.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    class="databox rounded border d-flex flex-column py-auto align-items-center justify-content-center"
+  >
+    <header>{{ title }}</header>
+    <p class="percentage-data" :class="getColour()" style="font-weight: bold">
+      {{ addUnits ? `${displayValue} % / Year` : displayValue }}
+    </p>
+    <p class="bench-mark">{{ benchMark }}</p>
+    <p class="value" :class="getColour()">{{ value }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "data-box",
+  props: {
+    title: String,
+    displayValue: Number,
+    benchMark: String,
+    value: Number,
+  },
+  data() {
+    return {
+      addUnits: false,
+    };
+  },
+  methods: {
+    getColour() {
+      if (this.title == "Standard Deviation" || this.title == "Sharpe Ratio") {
+        return "text-dark";
+      } else if (this.displayValue > 0) {
+        return "text-success";
+      } else if (this.displayValue < 0) {
+        return "text-danger";
+      }
+    },
+  },
+  created() {
+    if (this.title === "Price Return" || this.title === "Active Return") {
+      this.addUnits = true;
+    }
+  },
+};
+</script>
+<style scoped>
+.databox {
+  width: 300px;
+  height: 100px;
+  text-align: center;
+}
+p {
+  margin: 0;
+  font-size: medium;
+  font-weight: bold;
+}
+.percentage-data {
+  font-size: x-large;
+}
+header {
+  font-size: medium;
+}
+.bench-mark {
+  font-weight: normal;
+  color: gray;
+}
+</style>
--- oop/src/views/PortfolioPage.vue
+++ oop/src/views/PortfolioPage.vue
@@ -4,8 +4,21 @@
       <h1>Portfolio 1</h1>
     </div>
 
-    <div class="row-section border data-box">
-      component of boxes for stats should be here
+    <div class="row-section d-flex justify-content-between">
+      <data-box
+        title="Price Return"
+        :displayValue="percentageData"
+        :value="value"
+      >
+      </data-box>
+      <data-box title="Standard Deviation" :displayValue="stdDev"> </data-box>
+      <data-box title="Sharpe Ratio" :displayValue="sharpeRatio"> </data-box>
+      <data-box
+        title="Active Return"
+        :displayValue="activeReturn"
+        :bench-mark="benchMark"
+      >
+      </data-box>
     </div>
 
     <div class="time-series-graph border data-box">time series graph</div>
@@ -17,14 +30,30 @@
       <div>assets pie chart</div>
     </div>
 
-    <div class="performance-attribution-graph border data-box"> performance-attribution-graph</div>
+    <div class="performance-attribution-graph border data-box">
+      performance-attribution-graph
+    </div>
 
     <div class="stocks border data-box">stocks</div>
   </main>
 </template>
 
 <script>
+import dataBox from "@/components/dataBox.vue";
 export default {
+  data() {
+    return {
+      percentageData: -258,
+      value: "$3,252,737.08",
+      stdDev: 15.43,
+      sharpeRatio: 2.5,
+      activeReturn: 159,
+      benchMark: "S&P stock name info",
+    };
+  },
+  components: {
+    dataBox,
+  },
   methods: {},
 };
 </script>
