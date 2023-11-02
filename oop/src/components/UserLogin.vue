ef3c33159eaea86d1bbcc67b56e4f3fb8a2b96c9
sticker99 <srikar.primary@gmail.com>
2023-11-02 12:37:18 +0800

    Added full url for registration
    Signed-off-by: sticker99 <srikar.primary@gmail.com>

--- oop/src/components/UserLogin.vue
+++ oop/src/components/UserLogin.vue
@@ -399,7 +399,7 @@ export default {
       this.showSpinner = true;
 
       axios
-        .post(`/auth/register`, {
+        .post(`https://azure-oop-smux-gateway.azuremicroservices.io/api/auth/register`, {
           firstName: this.firstName,
           lastName: this.lastName,
           email: this.regEmail,
