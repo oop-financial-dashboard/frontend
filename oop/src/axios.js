efcd884c5e5f137e04e218dc8ddf3f6492e51ee8
sticker99 <srikar.primary@gmail.com>
2023-11-03 10:04:54 +0800

    Changed register() to full URL
    Signed-off-by: sticker99 <srikar.primary@gmail.com>

--- oop/src/axios.js
+++ oop/src/axios.js
@@ -4,4 +4,4 @@ import axios from 'axios';
 // axios.defaults.baseURL = 'http://localhost:8088/api/';
 
 // For deployment
-axios.defaults.baseURL = 'https://azure-oop-smux-gateway.azuremicroservices.io/api';
+// axios.defaults.baseURL = 'https://azure-oop-smux-gateway.azuremicroservices.io/api';
