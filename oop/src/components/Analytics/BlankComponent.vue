08aed68d255e5d7ff19475b5991cb25473c3f9dd
xuezhen01 <xuezhen.ng.2020@scis.smu.edu.sg>
2023-11-08 19:06:23 +0800

    UI hot fixes

--- oop/src/components/Analytics/BlankComponent.vue
+++ oop/src/components/Analytics/BlankComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card w-full h-56 mr-4 bg-white rounded-xl">
     <div class="card-body my-2.5">
-      <div class="text-center">
+      <div class="text-center mt-2">
         <img src="@/assets/NoStats.svg" class="mx-auto" style="width: 100px; color: #A4A6B3" alt="" />
       </div>
       <div class="text-center text-lg font-normal" style="color:#BAB9B9">
