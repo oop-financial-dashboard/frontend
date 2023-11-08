806db930fb2d31c9e9b142bfcc98a051ff03513b
xuezhen01 <xuezhen.ng.2020@scis.smu.edu.sg>
2023-11-09 01:35:52 +0800

    minor UI changes

--- oop/src/views/HomeView.vue
+++ oop/src/views/HomeView.vue
@@ -47,9 +47,9 @@
                   <!-- Display loading spinner or skeleton UI while data is loading -->
                   <div class="mb-2 flex animate-pulse">
                     <div class="w-full">
-                      <h3 class="h-5 bg-gray-200 rounded-full dark:bg-gray-700" style="width: 80%;"></h3>
-                      <h5 class="mt-4 h-5 bg-gray-200 rounded-full dark:bg-gray-700" style="width: 40%;"></h5>
-                      <h5 class="mt-4 h-5 bg-gray-200 rounded-full dark:bg-gray-700" style="width: 20%;"></h5>
+                      <h3 class="h-5 bg-gray-400 rounded-md dark:bg-gray-700" style="width: 80%;"></h3>
+                      <h5 class="mt-4 h-5 bg-gray-400 rounded-md dark:bg-gray-700" style="width: 40%;"></h5>
+                      <h5 class="mt-4 h-5 bg-gray-400 rounded-md dark:bg-gray-700" style="width: 20%;"></h5>
                     </div>
                   </div>
                 </div>
@@ -177,7 +177,7 @@
       <div class="d-flex justify-content-between mb-3">
         <p class="text-xl font-bold my-2">My Portfolios</p>
 
-        <button class="btn btn-dark" @click="createPortfolio">
+        <button class="btn bg-slate-600	text-white " @click="createPortfolio">
           Create Portfolio
         </button>
       </div>
